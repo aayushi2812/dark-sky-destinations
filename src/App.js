7960c79f3b7eb4c2f3a0f8a67ff75abcfe914157
aayushi <aayushiparekh2812@gmail.com>
2025-02-16 00:23:46 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import sampleVideo from "./dsd.mp4";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Android Mobile Application - Dark Sky Destinations</h2>
+      <video width="100%" height="700" controls>
+      <source src={sampleVideo} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
     </div>
   );
 }
